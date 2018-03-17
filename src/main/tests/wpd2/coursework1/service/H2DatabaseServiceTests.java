38999b21c46311bc646fa7600e7af228d92c234b
Alex McBride <xandermcbride@gmail.com>
2018-03-17 15:42:16 +0000

    Added DatabaseService tests

---
+++ src/main/tests/wpd2/coursework1/service/H2DatabaseServiceTests.java
@@ -0,0 +1,60 @@
+package wpd2.coursework1.service;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class H2DatabaseServiceTests {
+    @Test
+    public void testConnect() throws SQLException {
+        DatabaseService db = new H2DatabaseService(DatabaseService.Mode.TEST);
+        try (Connection conn = db.connect()) {
+            assertFalse(conn.isClosed());
+        }
+    }
+
+    private boolean checkTableExists(DatabaseService db, String tableName) throws SQLException {
+        try (Connection conn = db.connect()) {
+            ResultSet result = conn.getMetaData().getTables(null, null, tableName.toUpperCase(), null);
+            return result.next();
+        }
+    }
+
+    @Test
+    public void testInitialize() throws SQLException {
+        DatabaseService db = new H2DatabaseService(DatabaseService.Mode.TEST);
+        db.initialize();
+        assertTrue(checkTableExists(db, "projects"));
+    }
+
+    @Test
+    public void testDestroy() throws SQLException {
+        DatabaseService db = new H2DatabaseService(DatabaseService.Mode.TEST);
+        db.initialize();
+        db.destroy();
+        assertFalse(checkTableExists(db, "projects"));
+    }
+
+    @Test
+    public void testSeed() throws SQLException {
+        DatabaseService db = new H2DatabaseService(DatabaseService.Mode.TEST);
+        db.initialize();
+        db.seed();
+
+        int count = 0;
+        try (Connection conn = db.connect(); Statement statement = conn.createStatement()) {
+            ResultSet result = statement.executeQuery("SELECT COUNT(*) FROM projects");
+            if (result.next()) {
+                count = result.getInt(1);
+            }
+        }
+        assertEquals(count, 10);
+    }
+}
