40f7cf7a9db3444e307f6cdc9d6f8a22b05f73bc
Alex McBride <xandermcbride@gmail.com>
2018-03-17 15:57:21 +0000

    Added IoC tests

---
+++ src/main/tests/wpd2/coursework1/util/IoCTests.java
@@ -0,0 +1,43 @@
+package wpd2.coursework1.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class IoCTests {
+    @Test
+    public void testGet() {
+
+    }
+
+    @Test
+    public void testRegisterInstance() {
+        Object object = new Object();
+        IoC.get().registerInstance(Object.class, object);
+
+        Object result = IoC.get().getInstance(Object.class);
+
+        assertEquals(object, result);
+    }
+
+    @Test
+    public void testGetInstance() {
+        Object object = new Object();
+        IoC.get().registerInstance(Object.class, object);
+
+        Object result = IoC.get().getInstance(Object.class);
+
+        assertEquals(object, result);
+    }
+
+    @Test
+    public void testUnregisterInstance() {
+        IoC.get().registerInstance(Object.class, new Object());
+        IoC.get().unregisterInstance(Object.class);
+
+        Object result = IoC.get().getInstance(Object.class);
+
+        assertNull(result);
+    }
+}
