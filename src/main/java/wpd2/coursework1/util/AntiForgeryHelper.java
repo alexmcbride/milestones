e12a2aca791959178f19c0dff1a513e437032cd1
Alex McBride <xandermcbride@gmail.com>
2018-03-20 08:43:39 +0000

    CSRF helper now removes token from session once done

--- src/main/java/wpd2/coursework1/util/AntiForgeryHelper.java
+++ src/main/java/wpd2/coursework1/util/AntiForgeryHelper.java
@@ -19,6 +19,7 @@ public class AntiForgeryHelper {
 
     public void checkToken(String tokenToCheck) {
         String token = (String)session.getAttribute(KEY_FORGERY_TOKEN);
+        session.removeAttribute(KEY_FORGERY_TOKEN); // !important
         if (token == null || !token.equals(tokenToCheck)) {
             String message = "You must include the antiForgeryToken() form helper";
             throw new AntiForgeryException(message);
