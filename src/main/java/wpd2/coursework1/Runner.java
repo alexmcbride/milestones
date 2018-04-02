e1e7750495c0c611fe2ff1134e3f1a248312f409
Alex McBride <xandermcbride@gmail.com>
2018-04-02 15:51:01 +0100

    Fixed some issues with missing unit tests

--- src/main/java/wpd2/coursework1/Runner.java
+++ src/main/java/wpd2/coursework1/Runner.java
@@ -20,7 +20,7 @@ public class Runner {
     private static final Logger LOG = LoggerFactory.getLogger(Runner.class);
 
     private static final int PORT = 9000;
-    private static final boolean RESET_DATABASE_ON_STARTUP = true;
+    private static final boolean RESET_DATABASE_ON_STARTUP = false;
 
     private void start() throws Exception {
         initializeServices();
