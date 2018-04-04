f89279b321407132b8f53df77a0fa870df71bc72
Alex McBride <xandermcbride@gmail.com>
2018-04-04 17:19:39 +0100

    Added viewed to SharedProject

--- src/main/java/wpd2/coursework1/model/SharedProject.java
+++ src/main/java/wpd2/coursework1/model/SharedProject.java
@@ -9,6 +9,7 @@ public class SharedProject extends BaseModel {
     private int projectId;
     private int userId;
     private Date shared;
+    private Date viewed;
 
     public int getProjectId() {
         return projectId;
@@ -22,6 +23,14 @@ public class SharedProject extends BaseModel {
         return shared;
     }
 
+    public Date getViewed() {
+        return viewed;
+    }
+
+    public void setViewed(Date viewed) {
+        this.viewed = viewed;
+    }
+
     public void create(Project project, User user) {
         shared = new Date();
         projectId = project.getId();
@@ -33,6 +42,19 @@ public class SharedProject extends BaseModel {
             sta.setInt(2, userId);
             sta.setTimestamp(3, new Timestamp(shared.getTime()));
             sta.executeUpdate();
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void update() {
+        String sql = "UPDATE sharedProjects SET viewed=? WHERE userId=? AND projectId=?";
+        try (Connection conn = getConnection(); PreparedStatement statement = conn.prepareStatement(sql)) {
+            statement.setTimestamp(1, new Timestamp(viewed.getTime()));
+            statement.setInt(2, getUserId());
+            statement.setInt(3, getProjectId());
+            statement.executeUpdate();
         }
         catch (SQLException e) {
             throw new RuntimeException(e);
@@ -56,6 +78,7 @@ public class SharedProject extends BaseModel {
         sharedProject.projectId = result.getInt(1);
         sharedProject.userId = result.getInt(2);
         sharedProject.shared = result.getTimestamp(3);
+        sharedProject.viewed = result.getTimestamp(4);
         return sharedProject;
     }
 
@@ -64,7 +87,7 @@ public class SharedProject extends BaseModel {
     }
 
     public static SharedProject find(int userId, int projectId) {
-        String sql = "SELECT projectId, userId, shared FROM sharedProjects WHERE projectId=? AND userId=?";
+        String sql = "SELECT projectId, userId, shared, viewed FROM sharedProjects WHERE projectId=? AND userId=?";
         try (Connection conn = getConnection(); PreparedStatement sta = conn.prepareStatement(sql)) {
             sta.setInt(1, projectId);
             sta.setInt(2, userId);
@@ -84,7 +107,7 @@ public class SharedProject extends BaseModel {
     }
 
     public static List<SharedProject> findForUser(int userId) {
-        String sql = "SELECT projectId, userId, shared FROM sharedProjects WHERE userId=?";
+        String sql = "SELECT projectId, userId, shared, viewed FROM sharedProjects WHERE userId=?";
         return getSharedProjects(userId, sql);
     }
 
@@ -93,7 +116,7 @@ public class SharedProject extends BaseModel {
     }
 
     public static List<SharedProject> findForProject(int projectId) {
-        String sql = "SELECT projectId, userId, shared FROM sharedProjects WHERE projectId=?";
+        String sql = "SELECT projectId, userId, shared, viewed FROM sharedProjects WHERE projectId=?";
         return getSharedProjects(projectId, sql);
     }
 
@@ -117,7 +140,8 @@ public class SharedProject extends BaseModel {
             sta.execute("CREATE TABLE IF NOT EXISTS sharedProjects ( " +
                     "projectId INTEGER NOT NULL," +
                     "userId INTEGER NOT NULL," +
-                    "shared TIMESTAMP NOT NULL" +
+                    "shared TIMESTAMP NOT NULL," +
+                    "viewed TIMESTAMP NULL" +
                     ")");
         }
         catch (SQLException e) {
