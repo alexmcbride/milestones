3a4d0d8bdb020f6f9e459afe2c7839e1a3ef2e4d
Alex McBride <xandermcbride@gmail.com>
2018-04-06 15:29:22 +0100

    Fixed anti forgery stuff

--- src/main/java/wpd2/coursework1/util/AntiForgeryHelper.java
+++ src/main/java/wpd2/coursework1/util/AntiForgeryHelper.java
@@ -2,6 +2,12 @@ package wpd2.coursework1.util;
 
 import java.util.UUID;
 
+/*
+ * Protects against CSRF by using a synchronizer token. We generate a token and store it in
+ * the user's session, unique to that session. Each time we create a form we store this token
+ * in a hidden input, which we then check on submit. This prevents people spoofing the form.
+ * https://www.owasp.org/index.php/Cross-Site_Request_Forgery_(CSRF)_Prevention_Cheat_Sheet
+ */
 public class AntiForgeryHelper {
     public static final String KEY_FORGERY_TOKEN = "anti-forgery-token";
     private final SessionWrapper session;
@@ -21,9 +27,8 @@ public class AntiForgeryHelper {
 
     public void checkToken(String tokenToCheck) {
         String token = (String)session.getAttribute(KEY_FORGERY_TOKEN);
-        session.removeAttribute(KEY_FORGERY_TOKEN); // !important
         if (token == null || !token.equals(tokenToCheck)) {
-            String message = "You must include the antiForgeryToken() form helper";
+            String message = "Request blocked due to anti-forgery token check";
             throw new AntiForgeryException(message);
         }
     }
