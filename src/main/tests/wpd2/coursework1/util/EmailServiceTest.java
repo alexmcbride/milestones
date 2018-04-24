f71debb6f81f1bb5fc27023515633e7fab7958ff
Mie Tanaka <mietta25@gmail.com>
2018-04-24 21:43:36 +0100

    tempUser Email service test comit 2

--- src/main/tests/wpd2/coursework1/util/EmailServiceTest.java
+++ src/main/tests/wpd2/coursework1/util/EmailServiceTest.java
@@ -1,5 +1,15 @@
+package wpd2.coursework1.util;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmailServiceTest {
 
+
+    @Test
+    void testSendEmailUsingGMailSMTP() {
+        EmailService em = new EmailService();
+        assertTrue(em.SendEmailUsingGMailSMTP("mietta25@gmail.com","test","test message"));
+    }
 }
