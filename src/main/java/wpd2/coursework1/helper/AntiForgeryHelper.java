eac9f9af3b467b65157e3a2da64f0b31360d4b2d
Alex McBride <xandermcbride@gmail.com>
2018-04-25 13:36:32 +0100

    Tweak to CSRF helper

--- src/main/java/wpd2/coursework1/helper/AntiForgeryHelper.java
+++ src/main/java/wpd2/coursework1/helper/AntiForgeryHelper.java
@@ -38,6 +38,7 @@ public class AntiForgeryHelper {
     public void checkToken(String tokenToCheck) {
         String token = (String)session.getAttribute(KEY_FORGERY_TOKEN);
         if (token == null || !token.equals(tokenToCheck)) {
+            session.removeAttribute(KEY_FORGERY_TOKEN); // Remove key if not valid.
             String message = "Request blocked due to anti-forgery token check";
             throw new AntiForgeryException(message);
         }
